943b58f97a41781d48819e7a1ca99a55a45e7cac
Illia Korzun <iluhakorzun@gmail.com>
2020-11-03 00:15:37 +0200

    Add comments to key places in the code

--- sync_semaphore.cpp
+++ sync_semaphore.cpp
@@ -16,14 +16,22 @@ void produce(std::queue<std::string> &tasks)
 {
     while (true)
     {
+        /** postpone the thread execution 
+         * to simulate randomness of the appearance of a new task */
         std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 2000));
         auto task_to_produce = "task #" + std::to_string((rand() % 1000));
 
+        /** Wait until the consumer is done completing a task
+         * by attempting to decrement the semaphore's count
+        */
         produce_sem.acquire();
         {
+            /** add a new task atomically to the tasks queue*/
             std::lock_guard<std::mutex> lock(tasks_mtx);
             tasks.emplace(task_to_produce);
         }
+        /** signal the consumer to take a new task 
+         * if it is free */
         consume_sem.release();
 
         std::cout << "producer: added " << task_to_produce
@@ -38,14 +46,22 @@ void consume(std::queue<std::string> &tasks)
     {
         auto task_to_consume = std::string{};
 
+        /** Wait until the producer adds the task
+         * by attempting to decrement the semaphore's count
+        */
         consume_sem.acquire();
         {
+            /** pop a task safely from the tasks queue */
             std::lock_guard<std::mutex> lock(tasks_mtx);
             task_to_consume = tasks.front();
             tasks.pop();
         }
+        /** signal the producer to add a new task 
+         * if it has any */
         produce_sem.release();
 
+        /** postpone the thread execution 
+         * to simulate the task completion */
         std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 1500));
         std::cout << "consumer: finished " << task_to_consume
                   << " total(" << tasks.size() << ")"
