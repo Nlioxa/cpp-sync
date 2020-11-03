b35395b993633e55efd70bf53e9f806bb1ade803
Illia Korzun <iluhakorzun@gmail.com>
2020-11-03 13:40:14 +0200

    Notify a condition variable

--- sync_condition_variable_sequence.cpp
+++ sync_condition_variable_sequence.cpp
@@ -13,8 +13,10 @@ void consume(bool& is_task) {
     #ifdef USE_SYNC
     std::unique_lock<std::mutex> lock(transfer_mutex);
     cond.wait(lock, [&]{ return is_task; });
-    #endif
+    #else
+    
     if (is_task) 
+    #endif
     {
         std::cout << "consumer: complete task\n";
         is_task = false;
@@ -25,8 +27,13 @@ void produce(bool& is_task) {
     #ifdef USE_SYNC
     std::lock_guard<std::mutex> lock(transfer_mutex);
     #endif
+
     std::cout << "producer: create task\n";
     is_task = true;
+    
+    #ifdef USE_SYNC
+    cond.notify_one();
+    #endif
 }
 
 int main() {
